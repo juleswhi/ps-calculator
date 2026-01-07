bdba0599456e7dc9aa8b025b4cba941558e5cf12
callum white <callum@callumwhite.dev>
2026-01-07 17:06:39 +0100

    add questions and present them

---
+++ questions/questions.ts
@@ -0,0 +1,39 @@
+export enum AnswerType {
+    Continue,
+    SkipTo,
+    Answer
+}
+
+type Question = {
+    question: string,
+    prompt_one: string,
+    prompt_two: string,
+    one: AnswerType,
+    two: AnswerType,
+    val_one: number | null
+    val_two: number | null
+}
+
+export const Questions: Question[] = [
+    { question: "Is the patient alive?",
+        one: AnswerType.Continue, two: AnswerType.Answer, val_one: null, val_two: 5, prompt_one: "Yes", prompt_two: "No" }, // 0
+    { question: "Is the patient completely confined to bed or chair?",
+        one: AnswerType.Answer, two: AnswerType.Continue, val_one: 4, val_two: null, prompt_one: "Yes", prompt_two: "No" }, // 1
+    { question: "Does the patient spend more than 50% of waking hours in bed or chair?",
+        one: AnswerType.Continue, two: AnswerType.SkipTo, val_one: null, val_two: 6, prompt_one: "Yes", prompt_two: "No" }, // 2,
+    { question: "While spending > 50% of waking hours in bed or chair, can the patient perform any selfcare?",
+        one: AnswerType.Answer, two: AnswerType.Answer, val_one: 3, val_two: 4, prompt_one: "Only limited selfcare", prompt_two: "No selfcare at all" }, // 3
+    { question: "Is the patient able to get out of bed/chair and walk independently?",
+        one: AnswerType.Continue, two: AnswerType.Answer, val_one: null, val_two: 3, prompt_one: "Yes", prompt_two: "No" }, // 4
+    { question: "Is the patient up and about for more than 50% of waking hours?",
+        one: AnswerType.Continue, two: AnswerType.Answer, val_one: null, val_two: 3, prompt_one: "Yes", prompt_two: "No" }, // 5
+    { question: "Is the patient able to carry out all selfcare activities independently?",
+        one: AnswerType.Continue, two: AnswerType.Answer, val_one: null, val_two: 3, prompt_one: "Yes", prompt_two: "No" }, // 6
+    { question: "Is the patient able to carry out work activities ( job, household, usual roles )?",
+        one: AnswerType.Continue, two: AnswerType.Answer, val_one: null, val_two: 2, prompt_one: "Yes", prompt_two: "No" }, // 7
+    { question: "Is the patient restricted only in physically strenuous activity (e.g., heavy lifting, sports)?",
+        one: AnswerType.Answer, two: AnswerType.Continue, val_one: 1, val_two: null, prompt_one: "Yes", prompt_two: "No" }, // 8
+    { question: "Is the patient fully active with no limitations compared to pre-illness status?",
+        one: AnswerType.Answer, two: AnswerType.Answer, val_one: 0, val_two: 1, prompt_one: "Yes", prompt_two: "No" } // 9
+
+];
